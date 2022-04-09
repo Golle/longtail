bec7fc4e75be6bd0eaa20bb1a33420dab0b9e595
Jens Andersson <jens.andersson5@gmail.com>
2022-04-09 06:50:47 +0200

    List all header files that we want to parse

--- src/CodeGen/Program.cs
+++ src/CodeGen/Program.cs
@@ -1,3 +1,20 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
-Console.WriteLine("Hello world");
+
+const string longtailBasePath = @"O:\tmp\longtail";
+var longtailLibPath = Path.Combine(longtailBasePath, "lib");
+var longtailHeaderPath = Path.Combine(longtailBasePath, "src", "longtail.h");
+
+var headerFiles = Directory.GetDirectories(longtailLibPath, "*", SearchOption.TopDirectoryOnly)
+    .SelectMany(lib => Directory.GetFiles(lib, "*.h", SearchOption.TopDirectoryOnly))
+    .Concat(new[] { longtailHeaderPath });
+
+
+foreach (var file in headerFiles)
+{
+    Console.WriteLine(file);
+}
+
+
