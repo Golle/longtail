41d14fefbf498eb33fd686d40d5ed8714168d196
Jens Andersson <jens.andersson5@gmail.com>
2022-04-14 06:22:41 +0200

    Started on a new lexer

--- src/CodeGen/Program.cs
+++ src/CodeGen/Program.cs
@@ -1,19 +1,55 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System;
+using CodeGen;
 using CodeGen.Logging;
 
 
 using var _ = Logger.Start();
 
-const string longtailBasePath = @"O:\tmp\longtail";
-var longtailLibPath = Path.Combine(longtailBasePath, "lib");
-var longtailHeaderPath = Path.Combine(longtailBasePath, "src", "longtail.h");
 
-var headerFiles = Directory.GetDirectories(longtailLibPath, "*", SearchOption.TopDirectoryOnly)
-    .SelectMany(lib => Directory.GetFiles(lib, "*.h", SearchOption.TopDirectoryOnly))
-    .Concat(new[] { longtailHeaderPath })
-    .ToArray();
+
+Console.WriteLine("Welcome to the interpreter!");
+while (true)
+{
+    Console.Write("> ");
+    var line = Console.ReadLine();
+    if (line == null)
+    {
+        continue;
+    }
+
+    var lexer = new Lexer(line);
+
+    Token token;
+    while ((token = lexer.Lex()).Kind != TokenKind.EndOfFile)
+    {
+        Console.WriteLine(token);
+    }
+    Console.WriteLine("End of file reached.");
+}
+
+
+
+
+//const string longtailBasePath = @"O:\tmp\longtail";
+//var longtailLibPath = Path.Combine(longtailBasePath, "lib");
+//var longtailHeaderPath = Path.Combine(longtailBasePath, "src", "longtail.h");
+
+//var headerFiles = Directory.GetDirectories(longtailLibPath, "*", SearchOption.TopDirectoryOnly)
+//    .SelectMany(lib => Directory.GetFiles(lib, "*.h", SearchOption.TopDirectoryOnly));
+//    //.Concat(new[] { longtailHeaderPath })
+
+//var includes = headerFiles.Select(h => $"#include \"{h}\"");
+//var tempFile = Path.GetTempFileName();
+//try
+//{
+//    File.WriteAllLines(tempFile, includes);
+//    var process = Process.Start("gcc", $" -xc \"{tempFile}\" -E -P -o con:");
+//    process.WaitForExit();
+//}
+//finally
+//{
+//    File.Delete(tempFile);
+//}
 
 
 //var lookupTable = new Dictionary<string, TokenType>()
