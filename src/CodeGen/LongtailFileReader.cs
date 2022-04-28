8c99479cc48d45c50de4fa790f8c0b1c6b4da315
Jens Andersson <jens.andersson5@gmail.com>
2022-04-28 06:44:31 +0200

    Added enumerater for longtail header files

---
+++ src/CodeGen/LongtailFileReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CodeGen.Logging;
+
+namespace CodeGen;
+
+internal class LongtailFileReader
+{
+    private readonly string _longtailPath;
+
+    public LongtailFileReader(string longtailPath)
+    {
+        _longtailPath = longtailPath;
+    }
+
+    public IEnumerable<string> EnumerateAllHeaders(params (string Value, string? NewValue)[] replacements)
+    {
+        Logger.Info($"Reading header files in {_longtailPath}");
+        var longtailLibPath = Path.Combine(_longtailPath, "lib");
+        var longtailHeader = Path.Combine(_longtailPath, "src", "longtail.h");
+
+        var headerFiles = Directory.GetDirectories(longtailLibPath, "*", SearchOption.TopDirectoryOnly)
+            .SelectMany(lib => Directory.GetFiles(lib, "*.h", SearchOption.TopDirectoryOnly))
+            .ToArray();
+
+        
+        Logger.Trace($"Read contents of: {longtailHeader}");
+        yield return ReplaceContentes(File.ReadAllText(longtailHeader));
+        foreach (var headerFile in headerFiles)
+        {
+            Logger.Trace($"Read contents of: {headerFile}");
+            yield return ReplaceContentes(File.ReadAllText(headerFile));
+        }
+
+        string ReplaceContentes(string input)
+        {
+            // NOTE(Jens): this is really slow, allocating a new string for each replacement
+            foreach (var (value, newValue) in replacements)
+            {
+                input = input.Replace(value, newValue);
+            }
+            return input;
+        }
+    }
+
+}
--- src/CodeGen/Program.cs
+++ src/CodeGen/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CodeGen;
 using CodeGen.Logging;
@@ -8,6 +9,25 @@ using CodeGen.Syntax;
 
 
 using var _ = Logger.Start();
+
+const string LongtailPath = @"O:\tmp\longtail";
+
+//var longtailLibPath = Path.Combine(LongtailPath, "lib");
+//var longtailHeader = Path.Combine(LongtailPath, "src", "longtail.h");
+
+//var headerFiles = Directory.GetDirectories(longtailLibPath, "*", SearchOption.TopDirectoryOnly)
+//    .SelectMany(lib => Directory.GetFiles(lib, "*.h", SearchOption.TopDirectoryOnly))
+//    .ToArray();
+
+
+//foreach (var headerFile in headerFiles)
+//{
+//    Logger.Trace($"Reading contents: {headerFile}");
+//    var t = File.ReadAllText(headerFile);
+//    new Parser(t).Parse();
+//}
+
+//return;
 
 Console.WriteLine("Welcome to the interpreter!");
 
@@ -23,13 +43,16 @@ while (true)
 
     if (line.Length == 0)
     {
-        line = File.ReadAllText(@"O:\tmp\sample.h");
+        Parse(File.ReadAllText(@"O:\tmp\sample.h"));
     }
     else if (line.StartsWith("#longtail"))
     {
-        line = File.ReadAllText(@"o:\tmp\longtail\src\longtail.h").Replace("LONGTAIL_EXPORT", string.Empty);
-        //line = new LongtailPreParser(@"O:\tmp\longtail")
-        //    .Run();
+        var headers = new LongtailFileReader(LongtailPath)
+            .EnumerateAllHeaders(("LONGTAIL_EXPORT", "__declspec(dllexport)"));
+        foreach (var input in headers)
+        {
+            Parse(input);
+        }
     }
     else if (line.StartsWith("#file"))
     {
@@ -42,33 +65,41 @@ while (true)
 
         if (File.Exists(splits[1]))
         {
-            line = File.ReadAllText(splits[1]);
+            Parse(File.ReadAllText(splits[1]));
         }
         else
         {
             Console.WriteLine($"File {splits[1]} does not exist.");
-            continue;
         }
     }
-
-    try
+    else
     {
-        var timer = Stopwatch.StartNew();
-        var result = new Parser(line)
-            .Parse();
-        timer.Stop();
-        Console.WriteLine("Syntax tree.");
-        foreach (var syntaxNode in result.GetChildren())
+        Parse(line);
+    }
+
+    static SyntaxTree? Parse(string input)
+    {
+        try
         {
-            syntaxNode.PrettyPrint(new SyntaxConsoleWriter());
-            Logger.Raw(Environment.NewLine);
-        }
+            var timer = Stopwatch.StartNew();
+            var result = new Parser(input)
+                .Parse();
+            timer.Stop();
+            Console.WriteLine("Syntax tree.");
+            foreach (var syntaxNode in result.GetChildren())
+            {
+                syntaxNode.PrettyPrint(new SyntaxConsoleWriter());
+                Logger.Raw(Environment.NewLine);
+            }
 
-        Logger.Raw($"End of file reached. ({timer.Elapsed.TotalMilliseconds:0.##} ms)");
-    }
-    catch (ParserException e)
-    {
-        Logger.Error($"Parser failed with message {e.Message}");
+            Logger.Raw($"End of file reached. ({timer.Elapsed.TotalMilliseconds:0.##} ms)");
+            return result;
+        }
+        catch (ParserException e)
+        {
+            Logger.Error($"Parser failed with message {e.Message}");
+        }
+        return null;
     }
 }
 
