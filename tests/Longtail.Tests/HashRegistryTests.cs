1e37be4e041dce27361e197bcfe45f1367ab6383
Jens Andersson <jens.andersson5@gmail.com>
2022-05-15 07:39:27 +0200

    Add build and tests for longtail

---
+++ tests/Longtail.Tests/HashRegistryTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Longtail.Tests;
+
+internal unsafe class HashRegistryTests
+{
+    [Test]
+    public void HashRegistry_Blake3_CreateAndDispose()
+    {
+        var result = LongtailLibrary.Longtail_CreateBlake3HashRegistry();
+        
+        Assert.That(result != null);
+        
+        LongtailLibrary.Longtail_DisposeAPI((Longtail_API*)result);
+    }
+}
