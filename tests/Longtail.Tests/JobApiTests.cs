38b423d3848c4ad1b97a999623009af35ec85e8d
Jens Andersson <jens.andersson5@gmail.com>
2022-05-20 06:13:08 +0200

    Add JobApi managed class

---
+++ tests/Longtail.Tests/JobApiTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Longtail.Tests;
+
+internal class JobApiTests
+{
+    [Test]
+    public void CreateBikeshedJobAPI_Success_ReturnJobApi()
+    {
+        using var jobApi = JobApi.CreateBikeshedJobAPI(1);
+
+        Assert.That(jobApi, Is.Not.Null);
+    }
+
+    [TestCase(1u)]
+    [TestCase(4u)]
+    [TestCase(8u)]
+    public void CreateBikeshedJobAPI_WithWorkerCount_ReturnCorrectWorkerCount(uint workerCount)
+    {
+        using var jobApi = JobApi.CreateBikeshedJobAPI(workerCount);
+
+        var result = jobApi.GetWorkerCount();
+
+        Assert.That(result, Is.EqualTo(workerCount));
+    }
+}
