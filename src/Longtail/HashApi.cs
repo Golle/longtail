266da904be3600edbafa6d7d1fac5428e89f1b34
Jens Andersson <jens.andersson5@gmail.com>
2022-05-28 06:36:02 +0200

    Add GetIdentifier to HashApi

--- src/Longtail/HashApi.cs
+++ src/Longtail/HashApi.cs
@@ -5,6 +5,7 @@ public unsafe class HashApi : IDisposable
     private Longtail_HashAPI* _hashApi;
     private readonly bool _owner;
     internal Longtail_HashAPI* AsPointer() => _hashApi;
+    public uint GetIdentifier() => LongtailLibrary.Longtail_Hash_GetIdentifier(_hashApi);
     internal HashApi(Longtail_HashAPI* hashApi, bool owner = true)
     {
         _hashApi = hashApi;
