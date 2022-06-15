075c656e6976951f252f14a96ebce0e76bdb52a2
Jens Andersson <jens.andersson5@gmail.com>
2022-06-15 06:25:00 +0200

    Add more functions to StorageApi

--- src/Longtail/StorageApi.cs
+++ src/Longtail/StorageApi.cs
@@ -80,7 +80,95 @@ public unsafe class StorageApi : IDisposable
         return file != null ? new StorageApiOpenFile(_storageApi, file) : throw new InvalidOperationException($"{nameof(LongtailLibrary.Longtail_Storage_OpenWriteFile)} returned a null pointer");
     }
 
+    public void SetPermissions(string path, ushort permissions)
+    {
+        using var utf8Path = new Utf8String(path);
+        var err = LongtailLibrary.Longtail_Storage_SetPermissions(_storageApi, utf8Path, permissions);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_SetPermissions), err);
+        }
+    }
 
+    public ushort GetPermissions(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        ushort permissions;
+        var err = LongtailLibrary.Longtail_Storage_GetPermissions(_storageApi, utf8Path, &permissions);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_GetPermissions), err);
+        }
+        return permissions;
+    }
+
+    public void CreateDir(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        var err = LongtailLibrary.Longtail_Storage_CreateDir(_storageApi, utf8Path);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_CreateDir), err);
+        }
+    }
+
+    public void RenameFile(string sourcePath, string targetPath)
+    {
+        using var source = new Utf8String(sourcePath);
+        using var target = new Utf8String(targetPath);
+        var err = LongtailLibrary.Longtail_Storage_RenameFile(_storageApi, source, target);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_RenameFile), err);
+        }
+    }
+
+    public string ConcatPath(string rootPath, string subPath)
+    {
+        using var root = new Utf8String(rootPath);
+        using var sub = new Utf8String(subPath);
+        var result = LongtailLibrary.Longtail_Storage_ConcatPath(_storageApi, root, sub);
+        return Utf8String.GetString(result);
+    }
+
+    public string GetParentPath(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        var parentPath = LongtailLibrary.Longtail_Storage_GetParentPath(_storageApi, utf8Path);
+        return Utf8String.GetString(parentPath);
+    }
+
+    public bool IsDir(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        return LongtailLibrary.Longtail_Storage_IsDir(_storageApi, utf8Path) != 0;
+    }
+
+    public bool IsFile(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        return LongtailLibrary.Longtail_Storage_IsFile(_storageApi, utf8Path) != 0;
+    }
+
+    public void RemoveDir(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        var err = LongtailLibrary.Longtail_Storage_RemoveDir(_storageApi, utf8Path);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_RemoveDir), err);
+        }
+    }
+
+    public void RemoveFile(string path)
+    {
+        using var utf8Path = new Utf8String(path);
+        var err = LongtailLibrary.Longtail_Storage_RemoveFile(_storageApi, utf8Path);
+        if (err != 0)
+        {
+            throw new LongtailException(nameof(LongtailLibrary.Longtail_Storage_RemoveFile), err);
+        }
+    }
 
     public void Dispose()
     {
